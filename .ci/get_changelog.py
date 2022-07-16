38fe7049b8d2c3538b5cc28dba9306f75d576c98
Hans Dembinski <hans.dembinski@gmail.com>
2022-07-16 10:52:11 +0200

    u

--- .ci/get_changelog.py
+++ .ci/get_changelog.py
@@ -2,28 +2,44 @@ from pathlib import PurePath as Path
 import re
 import subprocess as subp
 from pkg_resources import parse_version
+import sys
+
+new_version = None
+latest_tag = None
+
+if len(sys.argv) > 1:
+    latest_tag = sys.argv[1]
+    latest_tag = parse_version(latest_tag)
+    if len(sys.argv) == 3:
+        new_version = sys.argv[2]
+        new_version = parse_version(new_version)
 
 project_dir = Path(__file__).parent.parent
 
-with open(project_dir / "version.py") as f:
-    version = {}
-    exec(f.read(), version)
-    new_version = parse_version(version["version"])
+if new_version is None:
+    with open(project_dir / "version.py") as f:
+        version = {}
+        exec(f.read(), version)
+        new_version = parse_version(version["version"])
 
-latest_tag = next(
-    iter(
-        sorted(
-            (
-                parse_version(x)
-                for x in subp.check_output(["git", "tag"]).decode().strip().split("\n")
-            ),
-            reverse=True,
+if latest_tag is None:
+    latest_tag = next(
+        iter(
+            sorted(
+                (
+                    parse_version(x)
+                    for x in subp.check_output(["git", "tag"])
+                    .decode()
+                    .strip()
+                    .split("\n")
+                ),
+                reverse=True,
+            )
         )
     )
-)
 
 # sanity checks
-assert new_version > latest_tag
+assert new_version > latest_tag, f"{new_version} > {latest_tag}"
 
 git_log = re.findall(
     r"[a-z0-9]+ ([^\n]+ \(#[0-9]+\))",
